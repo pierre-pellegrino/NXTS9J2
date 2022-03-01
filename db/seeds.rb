e0158a3d40b19d6e7882952ddfbf67668603dcaa
Pierre <pierre.pellegrino@laposte.net>
2022-03-01 14:00:50 +0100

    fixed seed bug with private column

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ require "faker"
 Article.delete_all
 User.delete_all
 
-User.create(email:"a@a.com", password:"123456")
+User.create(email:"a@a.com", password:"123456", private: false)
 
 30.times do |i|
   Article.create(title:Faker::Games::SuperMario.character, content: Faker::Games::SuperMario.game, user: User.first)
